2472de5fed2541c4bf28cadb1cacac790f976b92
Ricardo Souza <rcss2311@gmail.com>
2020-12-03 19:14:50 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,6 +32,11 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 3: seller findAll ===");
+		Seller newSeller = new Seller(null, "Greg", "gere@gmail.com", new Date(), 4000.00, department);
+		sellerdao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 
 	}
 	
