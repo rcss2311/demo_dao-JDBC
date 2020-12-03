9edb5da8249d31c0de1fe73ac1ead6c79a00bc63
Ricardo Souza <rcss2311@gmail.com>
2020-12-03 17:19:21 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
+		
+		System.out.println("=== Test 1: seller findById ===");
 		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
